ace0d88ed3dfe152e44a380a87425cdf3864af8e
Verissimo Santos <jose.sacramento@grupoboticario.com.br>
2023-08-29 12:02:57 -0300

    initial config

---
+++ src/main/java/com/aitech/strongBody/StrongBodyApplication.java
@@ -0,0 +1,13 @@
+package com.aitech.strongBody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrongBodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrongBodyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aitech/strongBody/StrongBodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitech.strongBody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrongBodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
