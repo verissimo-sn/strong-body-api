290b4b7ec12ff632cae58a55cf831c2249496b21
Verissimo Santos <jose.sacramento@grupoboticario.com.br>
2023-09-04 18:49:00 -0300

    test: added pageable util unit test

---
+++ src/test/java/com/aitech/strongBody/utils/PageableResponseMapperTest.java
@@ -0,0 +1,28 @@
+package com.aitech.strongBody.utils;
+
+import com.aitech.strongBody.document.ExerciseDocument;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class PageableResponseMapperTest {
+
+    @Test
+    void shouldReturnPageableEntityList() {
+        final var fakePageableList = new PageImpl(new ArrayList<>(List.of(
+                new ExerciseDocument()
+        )));
+        final var response = PageableResponseMapper.toPagination(fakePageableList);
+
+        assertEquals(fakePageableList.getContent(), response.get("data"));
+        assertEquals(fakePageableList.getNumber(), response.get("currentPage"));
+        assertEquals(fakePageableList.getTotalElements(), response.get("totalItems"));
+        assertEquals(fakePageableList.getTotalPages(), response.get("totalPages"));
+    }
+}
