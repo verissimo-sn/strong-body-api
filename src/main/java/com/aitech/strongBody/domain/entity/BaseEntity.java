171efde0214060171be18d945bd1550bf3afec56
Verissimo Santos <jose.sacramento@grupoboticario.com.br>
2023-09-10 20:37:21 -0300

    refactor: added ddd Entity

---
+++ src/main/java/com/aitech/strongBody/domain/entity/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.aitech.strongBody.domain.entity;
+
+import java.util.UUID;
+
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+public class BaseEntity {
+  private final UUID id;
+
+  public BaseEntity() {
+    this.id = UUID.randomUUID();
+  }
+
+  public BaseEntity(final UUID id) {
+    this.id = id;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+}
---
+++ src/main/java/com/aitech/strongBody/domain/entity/Exercise.java
@@ -0,0 +1,40 @@
+package com.aitech.strongBody.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@SuperBuilder
+public class Exercise extends BaseEntity {
+  private String name;
+  private String description;
+  private String level;
+  private String type;
+  private String equipment;
+  private String imageUrl;
+  private String videoUrl;
+
+  public Exercise updateExercise(
+    final String name,
+    final String description,
+    final String level,
+    final String type,
+    final String equipment,
+    final String imageUrl,
+    final String videoUrl
+  ) {
+    this.setName(name == null ? this.name : name);
+    this.setDescription(description == null ? this.description : description);
+    this.setLevel(level == null ? this.level : level);
+    this.setType(type == null ? this.type : type);
+    this.setEquipment(equipment == null ? this.equipment : equipment);
+    this.setImageUrl(imageUrl == null ? this.imageUrl : imageUrl);
+    this.setVideoUrl(videoUrl == null ? this.videoUrl : videoUrl);
+    return this;
+  }
+}
